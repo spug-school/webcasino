f9530bcc1d38d38564097a45f993605e38168add
kamm3r <marcokammer59@gmail.com>
2024-09-26 10:09:19 +0300

    signin and resigiter to almost done

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from cli.cli import CliTool
+from cli.main import Cmd
 from config import config
 
-def main():
-    foo = CliTool()
-    print(foo.parser.parse_args())
-    print(config())
+name = r"""
+  ____  _      ___    ____     _     ____  ___  _   _   ___
+ / ___|| |    |_ _|  / ___|   / \   / ___||_ _|| \ | | / _ \
+| |    | |     | |  | |      / _ \  \___ \ | | |  \| || | | |
+| |___ | |___  | |  | |___  / ___ \  ___) || | | |\  || |_| |
+ \____||_____||___|  \____|/_/   \_\|____/|___||_| \_| \___/
+"""
+
+def main()-> None:
+    print(name)
+    foo = Cmd()
+    selectedGame = foo.auth()
+    print(selectedGame)
+    # print(config())
 
 if __name__ == '__main__':
     main()
