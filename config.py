c3132a1e4a52c6022ae91f4f39ac8b7350597359
kamm3r <marcokammer59@gmail.com>
2024-09-26 10:23:38 +0300

    signin system testing

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ def config() -> dict:
             'db_port': os.getenv('DB_PORT', 3306),
             'db_user': os.getenv('DB_USER'),
             'db_pass': os.getenv('DB_PASS'),
+            'db_name': os.getenv('DB_NAME'),
             'autocommit': bool(os.getenv('DB_AUTOCOMMIT', False)),
-            'collation': 'utf8mb4_general_ci',   
+            'collation': 'utf8mb4_general_ci',
             'setup_file': os.getenv('DB_SETUP_FILE', None)
         },
     }
