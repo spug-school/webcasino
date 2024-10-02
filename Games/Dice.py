04ef0c404170806ec3408051f8d590056ba9b459
Tuukka <tuukka.lahdenpohja@metropolia.com>
2024-10-02 20:08:49 +0300

    Fixed compatibility issue with python 3.10.

--- Games/Dice.py
+++ Games/Dice.py
@@ -16,58 +16,54 @@ class Dice:
         max_dice (int): The maximum number of dice that can be rolled
         dice_sum (int): The sum of the dice rolls
     '''
-    def __init__(self, player: object, db_handler: object):
+    def __init__(self, player: dict):
         self.player = player
         self.name = 'Dice'
-        self.rules = 'Pelaaja valitsee itse, kuinka suurta noppaa heittää. Pelaajan tulee sitten arvata nopan oikea silmäluku.'
-        self.helpers = GameHelpers(player, {'name': self.name, 'rules': self.rules}, db_handler)
-        self.sides = 6 # default value
+        self.rules = 'The player has to guess the right value of the dice roll. The player himself determines the number of sides on the dice.'
+        self.sides = 6
+        self.helpers = GameHelpers(player)
 
-    def roll_dice(self) -> int:
+    def rollDice(self):
         '''
         Rolls the dice and returns the result
         '''
         return random.randint(1, self.sides)
+    
 
-    def determine_outcome(self, guess: int, roll: int, bet: int) -> int:
+    def determineOutcome(self, guess: int, roll: int, bet: int):
         '''
         Determines the outcome of the game
         '''
         return bet * self.sides if guess == roll else 0
 
-    def start_game(self) -> dict:
+    def startGame(self) -> dict:
         '''
         Runs the game and returns the player object when done
         '''
-        print(f'Tervetuloa nopanheittoon, {self.player.get_username()}!\n')
-        print(f'Pelin säännöt: {self.rules}\n')
-        
+        print(f'Welcome to the Dice game, {self.player['username']}!\n')
+
         while True:
-            bet = self.helpers.get_bet()
-            self.sides = int(input(f'Montako sivua nopassa on: '))
+            bet = self.helpers.getBet()
+            self.sides = int(input(f'Enter the number of sides on the dice: '))
 
             if self.sides < 2:
-                print('Nopassa on oltava yli 2 sivua. Syötä muu luku.\n')
+                print('The dice must have at least 2 sides. Please enter a valid number of sides.\n')
                 continue
 
-            guess = int(input(f'Syötä arvauksesi (1 - {self.sides}): '))
-            roll = self.roll_dice()
-            
-            outcome = self.determine_outcome(guess, roll, bet)
-            game_won = outcome > 0
+            guess = int(input(f'Enter your guess (1 - {self.sides}): '))
+            roll = self.rollDice()
+            outcome = self.determineOutcome(guess, roll, bet)
 
             if outcome > 0:
-                print(f'\nOnnittelut! Arvasit oikein! Voitit {outcome} pistettä!\n')
+                print(f'\nCongratulations! You guessed right! You have won {outcome}!\n')
+                self.helpers.updatePlayerBalance(outcome, getBalance=True)
             else:
-                print(f'\nHävisit pelin. Oikea arvo oli {roll}.\n')
-                
-            # Bulk-update the player values
-            self.helpers.update_player_values(game_won, outcome, save = True)
-            
-            # Save the game to the database
-            self.helpers.save_game_to_history(bet = bet, win_amount = outcome)
-            
-            if not self.helpers.play_again():
+                print(f'\nYou have lost the game. The dice roll was {roll}.\n')
+
+            if not self.helpers.playAgain():
                 break
 
-        return self.player # return the updated player object
+        return self.player
+            
+# game = Dice({'id': 2, 'username': 'John', 'balance': 125})
+# game.startGame()
--- Games/GameHelpers.py
+++ Games/GameHelpers.py
@@ -1,6 +1,3 @@
-import logging
-from datetime import datetime
-
 class GameHelpers:
     '''
     Helpers: Setup some common game methods that can be used across different games. Should be only used within the game classes.
@@ -8,109 +5,42 @@ class GameHelpers:
     Attributes:
         player (dict): The current player object
     '''
-    def __init__(self, player: object, game_info: dict, db_handler: object):
-        self.player = player # the whole class with the methods included
-        self.db = db_handler
-        self.game_info = game_info
-        
-        # Create the game type in the database
-        self.create_game_type()
+    def __init__(self, player: dict):
+        self.player = player
 
-    # Game related methods
-    def get_bet(self) -> int:
-        bet = int(input('\nPanos: '))
+    def getBet(self) -> int:
+        bet = int(input('Enter your bet: '))
         
-        if bet <= self.player.get_balance():
-            self.player.update_balance(-bet)
+        if bet <= self.player['balance']:
+            self.updatePlayerBalance(bet * -1)
             return bet
         else:
-            print(f'Saldosi on vajaa! Syötä sopiva määrä.\nSaldo: {self.player.get_balance()}')
-            return self.get_bet()
+            print(f'Insufficient balance! Please enter a valid bet.\nCurrent balance: {self.player['balance']}')
+            return self.getBet()
+
+    def updatePlayerBalance(self, amount: int, getBalance: bool = False) -> int:
+        self.player['balance'] += amount
+        if getBalance:
+            print(f'Current balance: {self.getCurrentBalance()}')
+        return self.player['balance']
+    
+    def getCurrentBalance(self) -> int:
+        return self.player['balance']
+
+    def playAgain(self) -> bool:
+        print('\n')
         
-    def play_again(self) -> bool:
-        if self.player.get_balance() <= 0:
-            print(f'Saldo ei riitä. Peli päättyi.\n')
+        if self.player['balance'] <= 0:
+            print(f'Out of balance! Cant play anymore.')
             return False
     
-        prompt = str(input('Pelataanko uudestaan (k / e): ')).lower()
+        prompt = input('Play again? (y/n): ').lower()
 
         match prompt:
-            case 'k':
+            case 'y':
                 return True
-            case 'e':
+            case 'n':
                 return False
             case _:
-                print(f'Virheellinen syöte. Syötä k tai e.')
-                return self.play_again()
-
-    # Database related methods
-    def create_game_type(self):
-        '''
-        Creates a new game type in the database. References the class name as the game type name
-        '''
-        try:
-            exists_query = 'SELECT * FROM game_types WHERE name = %s'
-            check_existance = self.db.query(exists_query, (self.game_info.get('name'),), cursor_settings={'dictionary': True})
-            
-            if len(check_existance['result']) > 0:
-                return
-            
-            query = 'INSERT INTO game_types (name, rules) VALUES (%s, %s)'
-            values = (self.game_info.get('name'), self.game_info.get('rules'))
-            
-            self.db.query(query, values)
-        except Exception as error:
-            logging.error(f'Error creating the game type: {error}')
-            return False
-    
-    def update_player_values(self, won: bool, win_amount = int, save = True):
-        '''
-        Updates all the player values in bulk after a game has ended
-        
-        @param won: bool: Whether the player won the game or not
-        @param win_amount: int: The amount the player won
-        @param save: bool: Whether to save the updated values to the database or not
-        '''
-        if won: # win
-            self.player.update_total_winning(win_amount)
-            self.player.update_games_won()
-        else: # loss
-            self.player.update_games_lost()
-            
-            if self.player.get_balance() <= 0:
-                self.player.set_banned()
-
-        self.player.update_games_played()
-        self.player.update_balance(win_amount)
-        
-        if save: # save the updated values to the database
-            self.player.save()
-    
-    def save_game_to_history(self, win_amount: int, bet: int) -> bool:
-        '''
-        Saves the game to the database. Returns success state boolean
-        '''
-        # TODO - save the game to the "game_history" table
-        try:
-            query = '''
-                INSERT INTO game_history
-                (bet, win_amount, played_at, user_id, game_type_id)
-                VALUES (%s, %s, %s, %s, %s)
-            '''
-            
-            user_id = self.player.get_data().get('id')
-            game_type_id = self.db.query('SELECT id FROM game_types WHERE name = %s', (self.game_info.get('name'),), cursor_settings={'dictionary': True})['result'][0].get('id')
-
-            values = (bet, win_amount, datetime.now(), int(user_id), int(game_type_id))
-            
-            result = self.db.query(query, values)
-            
-            if result['affected_rows'] > 0:
-                return True
-            else:
-                raise Exception('Unexpected error saving the game')
-            
-            # TODO
-        except Exception as error:
-            logging.error(f'Error saving the game: {error}')
-            return False
+                print(f'Invalid input. Please enter either "y" or "n".')
+                return self.playAgain()
