92c568bb6d1a9ed0136e3dbc362344c377d92c58
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-10-04 10:19:10 +0300

    Add method to add a new user on the go

--- Player/Player.py
+++ Player/Player.py
@@ -1,14 +1,15 @@
 import mysql.connector
 import logging
+import hashlib
 
 class Player:
     '''
     Holds the player's data in a temporary Class, and handles the player's data manipulation.
     The current class is used to update the player's data in the database after/during each singular game.
     '''
-    def __init__(self, username: str, db_handler: object, player: dict = None):
+    def __init__(self, username: str, db_handler: object):
         self.__db = db_handler
-        self.__data = player if player else self.__load_existing(username)
+        self.__data = self.__load_existing(username) if self.__load_existing(username) else self.__create_new(username)
 
     def get_data(self) -> dict:
         return self.__data
@@ -31,14 +32,41 @@ class Player:
     # TODO logic that tracks each game session (start of the game - leaving the game)
 
     # Db methods
-    def __load_existing(self, username: str) -> dict|None:
+    def __load_existing(self, username: str):
+        '''
+        Handles loading the player's data from the database, if username matches
+        '''
         try:
             result = self.__db.query('SELECT * FROM users WHERE username = %s', (username,), cursor_settings={'dictionary': True})
-            return result[0] if result else False
+            result_data = result['result']
+            
+            if len(result_data) == 0:
+                return False
+            else:
+                return result_data[0] # returns the first row
         except mysql.connector.Error as error:
             logging.error(f'Error loading player {self.__data['id']} data: {error}')
             return False
 
+    def __create_new(self, username: str, password: str = '') -> dict:
+        '''
+        Handles creating a new player in the database
+        '''
+        # TODO password handling
+        try:
+            query = '''
+                INSERT INTO users 
+                (username, password)
+                VALUES (%s, %s)
+                '''
+            self.__db.query(query, (username, password))
+            self.__db.connection.commit()
+            logging.info(f'New player {username} created successfully')
+            return self.__load_existing(username)
+        except mysql.connector.Error as error:
+            logging.error(f'Error creating new player {username}: {error}')
+            return False
+    
     def save(self):
         try:
             self.__db.query('UPDATE users SET total_winnings = %s, games_played = %s, games_won = %s, games_lost = %s, balance = %s WHERE username = %s', (self.__data['total_winnings'], self.__data['games_played'], self.__data['games_won'], self.__data['games_lost'], self.__data['balance'], self.__data['username']))
