96d48020520dfc9d9c8b1677b59363ec8254d5e4
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-10-04 10:25:32 +0300

    Removed a unnecessary dict level on config

--- config.py
+++ config.py
@@ -5,14 +5,12 @@ def config() -> dict:
     load_dotenv()
 
     return {
-        'connection_params': {
-            'db_host': os.getenv('DB_HOST', 'localhost'),
-            'db_port': os.getenv('DB_PORT', 3306),
-            'db_user': os.getenv('DB_USER'),
-            'db_pass': os.getenv('DB_PASS'),
-            'db_name': os.getenv('DB_NAME', 'cli_casino'),
-            'autocommit': bool(os.getenv('DB_AUTOCOMMIT', False)),
-            'collation': 'utf8mb4_general_ci',
-            'setup_file': os.getenv('DB_SETUP_FILE', None)
-        },
+        'db_host': os.getenv('DB_HOST', 'localhost'),
+        'db_port': os.getenv('DB_PORT', 3306),
+        'db_user': os.getenv('DB_USER'),
+        'db_pass': os.getenv('DB_PASS'),
+        'db_name': os.getenv('DB_NAME', 'cli_casino'),
+        'autocommit': bool(os.getenv('DB_AUTOCOMMIT', False)),
+        'collation': 'utf8mb4_general_ci',
+        'setup_file': os.getenv('DB_SETUP_FILE', None)
     }
