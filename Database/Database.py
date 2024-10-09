cf36f93341ed3134f19c2da3e0538eaf20eeb980
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-10-10 01:11:11 +0300

    Update Database.py setup methods to work with new cmd setup args

--- Database/Database.py
+++ Database/Database.py
@@ -14,28 +14,31 @@ class Database:
         db = Database(config, connect=True, setup=True)\n
         db.query('SELECT * FROM users')
     '''
-    def __init__(self, config: dict, connect: bool = True, setup: bool = False):
+    def __init__(self, config: dict, connect: bool = True, setup: dict = {}):
         self.config = config
-        self.setup_file = config.get('setup_file', None)
-        self.connection = self.create_connection(setup=setup) if connect else None
+        self.setup_file = config.get('setup_file', 'setup.sql')
+        self.connection = self.create_connection(setup = True if setup else None) if connect else None
         
-        if setup:
-            self.setup_database()
+        if setup and setup.get('sql', None):
+            self.setup_database(
+                file_name=self.setup_file,
+                source_files=setup.get('source', [])
+            )
         
-    def create_connection(self, setup: bool = False) -> mysql.connector.connection.MySQLConnection:
+    def create_connection(self, additional_params: dict = None, setup: bool = False) -> mysql.connector.connection.MySQLConnection:
         '''
         Creates a connection to the db, and returns the connection object
         '''
         try: 
             return mysql.connector.connect(
-                host = self.config['db_host'],
-                port = self.config['db_port'],
-                user = self.config['db_user'],
-                password = self.config['db_pass'],
-                autocommit = self.config['autocommit'],
-                collation = self.config['collation'],
-                charset = 'utf8mb4',
-                database = self.config['db_name'] if not setup else None
+                host=self.config['db_host'],
+                port=self.config['db_port'],
+                user=self.config['db_user'],
+                password=self.config['db_pass'],
+                autocommit=self.config['autocommit'],
+                collation=self.config['collation'],
+                charset='utf8mb4',
+                database=self.config['db_name'] if not setup else None
             )
         except mysql.connector.Error as connection_error:
             logging.error(f'Error connecting to the database: {connection_error}')
@@ -46,7 +49,7 @@ class Database:
         Closes the connection to the db
         '''
         try:
-            self.connection.commit() # Commit any pending transactions
+            self.connection.commit()  # Commit any pending transactions
             self.connection.close()
             logging.info('Database connection closed')
             return True
@@ -54,25 +57,35 @@ class Database:
             logging.error(f'Error closing the database connection:\n{error}')
             return False
         
-    def setup_database(self, file_path: str = None) -> bool:
+    def setup_database(self, file_name: str, source_files: list = []) -> bool:
         '''
-        Runs the initital setup script for the db
+        Runs the initial setup script for the db
         '''
-        file_path = file_path if file_path else self.setup_file
-        
-        if file_path is None or file_path == '':
+        if file_name is None or file_name == '':
             logging.warning('No setup file provided')
+            print('No setup file provided')
             return False
         
         try:
-            with open(get_file_path(self.setup_file), 'r', encoding='utf-8') as setup_file:
+            with open(get_file_path(file_name), 'r', encoding='utf-8') as setup_file:
                 setup_script = setup_file.read()
                 self.execute_script(setup_script)
-                
+
             tables = [table[0] for table in self.query("SHOW TABLES")['result']]
-                        
+            
+            # Insert test data if provided
+            for source_file_name in source_files:
+                with open(get_file_path(source_file_name), 'r', encoding='utf-8') as source_file:
+                    source_script = source_file.read()
+                    self.execute_script(source_script)
+
             logging.info(f'Database `{self.connection.database}` setup successfully.\nTables: {tables}')
             box_wrapper(f'Database `{self.connection.database}` setup successfully.\nTables: {tables}')
+
+            if source_files:
+                logging.info(f'Source data inserted successfully')
+                box_wrapper(f'Source data inserted successfully')
+    
             return True
         except Exception as error:
             logging.error(f'Error setting up the database:\n{error}')
