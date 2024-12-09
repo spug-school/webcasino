4fd3773df431ba8412dff4de1a421f1a828aadb0
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2024-12-09 19:57:12 +0200

    leaderboard & profile pages done

--- web/src/pages/leaderboard.js
+++ web/src/pages/leaderboard.js
@@ -1,44 +1,108 @@
-export function Leaderboard(req) {
-  const root = document.querySelector("#root");
-  console.log(req);
-  root.innerHTML = `
-    <div class="table-container">
-    <table>
-    <thead>
-        <tr>
-            <th>Rank</th>
-            <th>Username</th>
-            <th>Balance</th>
-            <th>Games Played</th>
-            <th>Games Won</th>
-            <th>Games Lost</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>Bob Ross</td>
-            <td>100</td>
-            <td>10</td>
-            <td>5</td>
-            <td>5</td>
-         
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Bob Ross</td>
-            <td>100</td>
-            <td>10</td>
-            <td>5</td>
-            <td>5</td>
-          
-        </tr>
-  
-        <!-- Add more rows as needed -->
-  
-    </tbody>
-  
-  </table>
-  </div>
-  `;
+import { getLeaderBoardData } from "../utils/fetchUtils";
+
+
+export async function Leaderboard(req) {
+    const root = document.querySelector("#root");
+    root.innerHTML = `
+    <div class="container table-container">
+        <h1>Tulostaulut</h1>
+        <p>Valitse suodatin ja järjestys</p>
+        <form id="leaderboard-filter">
+            <select name="filter">
+                <option value="total_winnings" selected>Kokonaisvoitot</option>
+                <option value="games_played">Pelatut pelit</option>
+                <option value="games_won">Voitetut pelit</option>
+                <option value="games_lost">Hävityt pelit</option>
+            </select>
+            <select name="sort">
+                <option value="asc">Nouseva</option>
+                <option value="desc" selected>Laskeva</option>
+            </select>
+            <input type="submit" value="Suodata"/>
+        </form>
+        <table id="leaderboard">  
+        </table>
+    </div>
+    `;
+
+    initializeLeaderboard()
+}
+
+// matches the names from db query
+const tableColumns = [
+    'käyttäjänimi',
+    'kokonaisvoitot',
+    'pelejä pelattu',
+    'pelejä voitettu',
+    'pelejä hävitty',
+]
+
+function createTable(data, destination) {
+    destination.innerHTML = '';
+
+    const thead = document.createElement('thead');
+    const headTr = document.createElement('tr');
+    const tbody = document.createElement('tbody');
+
+    // create the header row
+    tableColumns.forEach(column => {
+        const th = document.createElement('th');
+        th.textContent = column;
+        headTr.appendChild(th);
+    });
+
+    thead.appendChild(headTr);
+
+    // create the data rows
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+        const sortedRow = sortObjectByOrder(row, tableColumns)
+
+        Object.values(sortedRow).forEach(column => {
+            const td = document.createElement('td');
+            td.textContent = column;
+            tr.appendChild(td);
+        });
+
+        tbody.appendChild(tr);
+    });
+
+    destination.appendChild(thead);
+    destination.appendChild(tbody);
+}
+
+function sortObjectByOrder(obj, order) {
+    return order.reduce((sorted, key) => {
+        if (obj.hasOwnProperty(key)) {
+            sorted[key] = obj[key];
+        }
+        return sorted;
+    }, {});
+}
+
+async function initializeLeaderboard() {
+    const table = document.querySelector('table#leaderboard')
+    const filterForm = document.querySelector('form#leaderboard-filter')
+
+    // listen to changes in the filtering
+    filterForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+
+        // get the form values
+        const filter = filterForm.querySelector('[name="filter"]').value
+        const sort = filterForm.querySelector('[name="sort"]').value
+
+        const filteredData = await getLeaderBoardData({
+            filter: filter,
+            sort: sort,
+        })
+
+        createTable(filteredData, table)
+    });
+
+    const initialData = await getLeaderBoardData({
+        filter: 'total_winnings',
+        sort: 'desc'
+    })
+    createTable(initialData, table)
 }
--- web/src/pages/profile.js
+++ web/src/pages/profile.js
@@ -1,32 +1,57 @@
-async function getData(url) {
-  const response = await fetch(url, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  return await response.json();
-}
+import { getGameHistory, getPlayerData } from "../utils/fetchUtils";
 
 export async function Profile(req) {
   const root = document.querySelector("#root");
-  const data = await getData("http://127.0.0.1:5000/api/profile");
+
+  const playerData = await getPlayerData(localStorage.getItem("user_id"));
+  console.log(playerData)
+  const gameHistory = await getGameHistory();
+
   root.innerHTML = `
-    <div id="title">
-    <h1>My profile</h1>
-    </div>
-    <div>
-    <p>ID: ${data.id}</p>
-    <p>Username: ${data.username}</p>
-    </div>
+    <div class="container">
+      <h2>Omat tiedot</h2>
+      <table id="profile-info">
+        <thead>
+          <tr>
+            <th>Käyttäjänimi</th>
+            <th>Saldo</th>
+            <th>Pelejä pelattu</th>
+            <th>Voitetut pelit</th>
+            <th>Hävityt pelit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>${playerData.username}</td>
+            <td>${playerData.balance}</td>
+            <td>${playerData.games_played}</td>
+            <td>${playerData.games_won}</td>
+            <td>${playerData.games_lost}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>Viimeisimmät pelisi</h2>
+      <table id="game-history">
+        <thead>
+          <tr>
+            <th>Peli</th>
+            <th>Ajankohta</th>
+            <th>Panos</th>
+            <th>Voiton määrä</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${gameHistory.map((game) => `
+              <tr>
+                <td>${game.name}</td>
+                <td>${game.played_at}</td>
+                <td>${game.bet}</td>
+                <td>${game.win_amount}</td
+              </tr>
+            `
+          ).join("")}
+        </tbody>
+      </table>
     </div>
     `;
-  root.appendChild(p);
-  console.log(data);
 }
