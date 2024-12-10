e5d241e4384d0092d76e19217511b6ea1041e787
Harles-A <harles.asu@gmail.com>
2024-12-10 20:15:07 +0200

    Did the Games page

--- web/src/pages/games.js
+++ web/src/pages/games.js
@@ -2,30 +2,46 @@ export function Games(req) {
   const root = document.querySelector("#root");
   console.log(req);
   root.innerHTML = `
-    <h2>Games</h2>
-    <p>Choose a game</p>
-    <div class="games">
-      <a href="/games/dice">
-      <figure>
-        <img 
-          src="https://cdn.pixabay.com/photo/2022/03/01/22/02/dice-7195733_960_720.png" 
-          alt="Dice">
-        <h3>Dice</h3>
-        <p>Description</p>
-      </figure>
-      </a>
-      <div>
-      <a href="/games/slots">
-        <h3>Slots</h3>
-        <p>Description</p>
+    <div class="games-container">
+      <h2>Games</h2>
+      <p>Choose a game</p>
+      <div class="games">
+        <div class="game-card">
+          <a href="/games/dice">
+            <img 
+              src="/images/games_page/dicegame.png" 
+              alt="Dice"
+              style="width: 100%; border-radius: 0.5rem;"
+            >
+            <h3>Dice</h3>
+            <p>Roll the dice and test your luck!</p>
+          </a>
+        </div>
+
+        <div class="game-card">
+          <a href="/games/slots">
+            <img 
+              src="/images/games_page/slotsgame.png" 
+              alt="Slots"
+              style="width: 100%; border-radius: 0.5rem;"
+            >
+            <h3>Slots</h3>
+            <p>Spin the reels and win big!</p>
+          </a>
+        </div>
+
+        <div class="game-card">
+          <a href="/games/roulette">
+            <img 
+              src="/images/games_page/roulettegame.png" 
+              alt="Roulette"
+              style="width: 100%; border-radius: 0.5rem;"
+            >
+            <h3>Roulette</h3>
+            <p>Place your bets and spin the wheel!</p>
+          </a>
+        </div>
       </div>
-      <div>
-        <h3>Roulette</h3>
-        <p>Description</p>
-      </div>
-      <div>
     </div>
-    <div>
-      
   `;
 }
