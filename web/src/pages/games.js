849e48c36644fcc8dcc329d31c6da9dd6b422a02
Harles-A <harles.asu@gmail.com>
2024-12-11 12:51:57 +0200

    added ventti and roulette to games page

--- web/src/pages/games.js
+++ web/src/pages/games.js
@@ -3,8 +3,8 @@ export function Games(req) {
   console.log(req);
   root.innerHTML = `
     <div class="games-container">
-      <h2>Games</h2>
-      <p>Choose a game</p>
+      <h2>Pelit</h2>
+      <p>Valitse Peli</p>
       <div class="games">
         <div class="game-card">
           <a href="/games/dice">
@@ -13,8 +13,8 @@ export function Games(req) {
               alt="Dice"
               style="width: 100%; border-radius: 0.5rem;"
             >
-            <h3>Dice</h3>
-            <p>Roll the dice and test your luck!</p>
+            <h3>Nopanheitto</h3>
+            <p>Heitä noppaa ja testaa onneasi!</p>
           </a>
         </div>
 
@@ -25,8 +25,8 @@ export function Games(req) {
               alt="Slots"
               style="width: 100%; border-radius: 0.5rem;"
             >
-            <h3>Slots</h3>
-            <p>Spin the reels and win big!</p>
+            <h3>Hedelmäpeli</h3>
+            <p>Vedä vivusta ja voita isosti!</p>
           </a>
         </div>
 
@@ -37,8 +37,32 @@ export function Games(req) {
               alt="Coin Flip"
               style="width: 100%; border-radius: 0.5rem;"
             >
-            <h3>Coin Flip</h3>
-            <p>Flip the coin and decide your fate!</p>
+            <h3>Kolikonheitto</h3>
+            <p>Heitä kolikko ja anna kohtalon päättää!</p>
+          </a>
+        </div>
+        
+        <div class="game-card">
+          <a href="/games/roulette">
+            <img 
+              src="/images/games_page/roulettegame.png" 
+              alt="Coin Flip"
+              style="width: 100%; border-radius: 0.5rem;"
+            >
+            <h3>Ruletti</h3>
+            <p>Pyörän ympäri kiertää, mihin onni laskeutuu, kuka tietää?</p>
+          </a>
+        </div>
+        
+        <div class="game-card">
+          <a href="/games/ventti">
+            <img 
+              src="/images/games_page/venttigame.png" 
+              alt="Coin Flip"
+              style="width: 100%; border-radius: 0.5rem;"
+            >
+            <h3>Ventti</h3>
+            <p>Pelaa korttisi oikein ja voita jakajan!</p>
           </a>
         </div>
       </div>
